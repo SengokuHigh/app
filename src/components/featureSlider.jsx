c697f0350895f7c191a650214d042eda8ca93e4f
YSJ <shaojieyew@gmail.com>
2021-10-22 00:08:18 +0800

    new update

--- src/components/featureSlider.jsx
+++ src/components/featureSlider.jsx
@@ -13,12 +13,14 @@ export const FeatureSlider = (props) => {
             <Slider slidesPerRow="3" autoplay="true" autoplaySpeed	="3000" dots>
             {props.data
                 ? props.data.features.map((d, i) => (
-                    <div key={`${d.title}-${i}`} className='col-xs-6 col-md-3 features-section'>
+                  <div  className='col-xs-6 col-md-3 features-section-wrapper'>
+                    <div key={`${d.title}-${i}`} className='features-section'>
                       {' '}
                       <img src={d.image ? d.image : ""} className="img-responsive" alt="" />
                       <h3>{d.title}</h3>
                       <p>{d.text}</p>
                     </div>
+                  </div>
                   ))
                 : 'Loading...'}
           </Slider>
